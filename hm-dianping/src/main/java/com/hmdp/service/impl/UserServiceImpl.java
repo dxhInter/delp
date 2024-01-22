670ad2a8eb62fad91c66f64cbf38f83ee1a794ff
dxhInter <dxh0530@gmail.com>
2024-01-21 20:34:35 -0500

    first commit

---
+++ hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -0,0 +1,68 @@
+package com.hmdp.service.impl;
+
+import cn.hutool.core.util.RandomUtil;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.User;
+import com.hmdp.utils.UserContents;
+import com.hmdp.mapper.UserMapper;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
+
+/**
+ * <p>
+ * 服务实现类
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2021-12-22
+ */
+@Service
+@Slf4j
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手机号格式不正确");
+        }
+        String code = RandomUtil.randomNumbers(6);
+        session.setAttribute(UserContents.CODE, code);
+        log.debug("手机号：{}，发送验证码成功：{}", phone, code);
+        return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        String phone = loginForm.getPhone();
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手机号格式不正确");
+        }
+        Object cacheCode = session.getAttribute(UserContents.CODE);
+        String code = loginForm.getCode();
+        if (cacheCode == null || !cacheCode.equals(code)) {
+            return Result.fail("验证码不正确");
+        }
+        User user = query().eq(UserContents.PHONE, phone).one();
+        if (user == null) {
+            user = createUserWithPhone(phone);
+        }
+        session.setAttribute(UserContents.PHONE, user);
+        return Result.ok();
+    }
+
+    private User createUserWithPhone(String phone) {
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomString(10));
+        save(user);
+        return user;
+    }
+}
---
+++ hm-dianping/src/main/java/com/hmdp/utils/UserContents.java
@@ -0,0 +1,8 @@
+package com.hmdp.utils;
+
+public class UserContents {
+    public static final String PHONE = "phone";
+    public static final String CODE = "code";
+    public static final String USER = "user";
+
+}
