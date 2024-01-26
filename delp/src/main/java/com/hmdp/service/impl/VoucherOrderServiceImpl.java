ff7af34297c5157125a5ec58a93c396c33fb0c46
dxhInter <dxh0530@gmail.com>
2024-01-26 13:51:16 -0500

    using @Transactional and synchronized to solve the problem of one person per order, which is a pessimistic locking strategy.

--- delp/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ delp/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -7,8 +7,10 @@ import com.hmdp.mapper.VoucherOrderMapper;
 import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.service.IVoucherService;
 import com.hmdp.utils.RedisIdWorker;
 import com.hmdp.utils.UserHolder;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -31,7 +33,6 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
     private RedisIdWorker redisIdWorker;
 
     @Override
-    @Transactional
     public Result seckillVoucher(Long voucherId) {
         SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
         if (voucher.getBeginTime().isAfter(LocalDateTime.now())) {
@@ -46,20 +47,39 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
             // 库存不足
             return Result.fail("库存不足");
         }
+        // 这里加intern是因为如果创建的String对象在常量池中已经存在，那么就直接返回常量池中的对象，不会在堆中创建新的对象, toString()默认返回String对象
+
+        //一人一单判断
+        Long userId = UserHolder.getUser().getId();
+        synchronized (userId.toString().intern()) {
+            // 要在事物提交后释放锁
+            // spring事物失效问题，所以要使用代理对象，自己注入自己，获取spring代理对象，Aop
+            IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
+            return proxy.createVoucherOrder(voucherId);
+        }
+    }
+
+    @Transactional
+    public Result createVoucherOrder(Long voucherId) {
+        Long userId = UserHolder.getUser().getId();
+        int count = query().eq("user_id", userId).eq("voucher_id", voucherId).count();
+        if (count > 0) {
+            // 用户已经购买过
+            return Result.fail("用户已经购买过了");
+        }
         // 扣减库存
         boolean success = seckillVoucherService
                 .update()
                 .setSql("stock = stock - 1") // stock = stock - 1
                 .eq("voucher_id", voucherId).gt("stock", 0) // voucher_id = voucherId and stock > 0
                 .update();
-        if(!success){
+        if (!success) {
             // 扣减失败
             return Result.fail("库存不足");
         }
         VoucherOrder voucherOrder = new VoucherOrder();
         long orderId = redisIdWorker.nextId("order");
         voucherOrder.setId(orderId);
-        Long userId = UserHolder.getUser().getId();
         voucherOrder.setUserId(userId);
         voucherOrder.setVoucherId(voucherId);
         save(voucherOrder);
